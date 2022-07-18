90f32ce5d7725c56a046061d598eda3a89b539c6
Tinyu <Tinyu.Zhao@gmail.com>
2022-07-18 10:54:23 +0800

    Update example

--- idf/RGB_LED_Control/README.md
+++ idf/RGB_LED_Control/README.md
@@ -1,2 +1,8 @@
 # STAMP-C3 IDF Example
 
+1. Use IDF to open the RGB_LED_CONTROL folder [RGB_LED_CONTROL](/idf/RGB_LED_CONTROL).
+2. Connect the STAMP C3 to the computer and select the corresponding serial port.
+3. Select ESP32C3 chip.
+4. Compile and burn and open the serial port.
+
+The test environment is ESP-IDF v4.4.1-472-gc9140caf8c
--- idf/RGB_LED_Control/main/main.c
+++ idf/RGB_LED_Control/main/main.c
@@ -27,7 +27,7 @@
 led_strip_t *strip;
 
 #ifdef STAMP_C3
-uint8_t PINS[] = {4, 5, 6, 7, 8, 10, 1, 0, 21, 20, 9, 18, 19};
+uint8_t PINS[] = {4, 5, 6, 7, 8, 10, 1, 0, 20, 9, 18, 19};
 #define BTN_PIN 3
 #endif
 
@@ -38,30 +38,30 @@ uint8_t PINS[] = {4, 5, 6, 7, 8, 10, 1, 0, 21, 20, 3, 18, 19};
 
 #define RMT_TX_GPIO 2
 
-void update_led(uint8_t r, uint8_t g, uint8_t b)
-{
+uint8_t brill_red   = 0;   // Initial brilliance
+uint8_t brill_green = 25;  // idem
+uint8_t brill_blue  = 25;  // idem
+#define brill 20           // inside main loop: max value for brilliance
+
+void update_led(uint8_t r, uint8_t g, uint8_t b) {
     ESP_ERROR_CHECK(strip->set_pixel(strip, 0, r, g, b));
     ESP_ERROR_CHECK(strip->refresh(strip, 0));
 }
 
 uint8_t state = 1;
 
-void io_reverse()
-{
+void io_reverse() {
     state = !state;
     ESP_LOGI("IO REVERSE", "IO STATE: %d", state);
-    for (uint8_t index = 0; index < sizeof(PINS); index++)
-    {
+    for (uint8_t index = 0; index < sizeof(PINS); index++) {
         gpio_set_level(PINS[index], state);
     }
 }
 
 uint8_t base_mac_addr[6] = {0};
 
-void app_main(void)
-{
-
-    //Init RMT
+void app_main(void) {
+    // Init RMT
     rmt_config_t config = RMT_DEFAULT_CONFIG_TX(RMT_TX_GPIO, RMT_TX_CHANNEL);
     // set counter clock to 40MHz
     config.clk_div = 2;
@@ -70,22 +70,22 @@ void app_main(void)
     ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 
     // install ws2812 driver
-    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(1, (led_strip_dev_t)config.channel);
+    led_strip_config_t strip_config =
+        LED_STRIP_DEFAULT_CONFIG(1, (led_strip_dev_t)config.channel);
     strip = led_strip_new_rmt_ws2812(&strip_config);
-    if (!strip)
-    {
+    if (!strip) {
         ESP_LOGE("Main", "Install WS2812 driver failed");
     }
 
-    //Get base MAC address from EFUSE BLK0(default option)
+    // Get base MAC address from EFUSE BLK0(default option)
     esp_efuse_mac_get_default(base_mac_addr);
 
-    ESP_LOGI("Main", "MAC: %X%X%X%X%X%X",
-             base_mac_addr[0], base_mac_addr[1], base_mac_addr[2], base_mac_addr[3], base_mac_addr[4], base_mac_addr[5]);
+    ESP_LOGI("Main", "MAC: %X%X%X%X%X%X", base_mac_addr[0], base_mac_addr[1],
+             base_mac_addr[2], base_mac_addr[3], base_mac_addr[4],
+             base_mac_addr[5]);
 
-    //INIT IO MODE
-    for (uint8_t index = 0; index < sizeof(PINS); index++)
-    {
+    // INIT IO MODE
+    for (uint8_t index = 0; index < sizeof(PINS); index++) {
         gpio_reset_pin(PINS[index]);
         gpio_set_direction(PINS[index], GPIO_MODE_OUTPUT);
         gpio_set_pull_mode(PINS[index], GPIO_PULLUP_ONLY);
@@ -94,26 +94,30 @@ void app_main(void)
     gpio_set_direction(BTN_PIN, GPIO_MODE_INPUT);
     gpio_set_pull_mode(BTN_PIN, GPIO_PULLUP_ONLY);
 
-    update_led(0, 255, 200);
+    // update_led(0, 255, 200);
+    ESP_LOGI("Main", "Initial LED brilliance set to: r(%d), g(%d) and b(%d)",
+             brill_red, brill_green, brill_blue);
+    update_led(brill_red, brill_green, brill_blue);
 
-    while (true)
-    {
-        if (gpio_get_level(BTN_PIN) == 0)
-        {
+    while (true) {
+        if (gpio_get_level(BTN_PIN) == 0) {
             vTaskDelay(5 / portTICK_RATE_MS);
-            if (gpio_get_level(BTN_PIN) == 0)
-            {
-                int r = rand() % 256;
-                int g = rand() % 256;
-                int b = rand() % 256;
+            if (gpio_get_level(BTN_PIN) == 0) {
+                int r = rand() % brill;  // was "% 256"
+                int g = rand() % brill;
+                int b = rand() % brill;
                 update_led(r, g, b);
                 io_reverse();
-                while (gpio_get_level(BTN_PIN) == 0)
-                {
+                while (gpio_get_level(BTN_PIN) == 0) {
                     srand((unsigned)b);
                 }
-                ESP_LOGI("Main", "MAC: %X%X%X%X%X%X",
-                         base_mac_addr[0], base_mac_addr[1], base_mac_addr[2], base_mac_addr[3], base_mac_addr[4], base_mac_addr[5]);
+                ESP_LOGI("Main", "MAC: %X%X%X%X%X%X", base_mac_addr[0],
+                         base_mac_addr[1], base_mac_addr[2], base_mac_addr[3],
+                         base_mac_addr[4], base_mac_addr[5]);
+                ESP_LOGI(
+                    "Main",
+                    "LED brilliance for r(%d), g(%d) and b(%d). Maximum: %d", r,
+                    g, b, brill);
             }
         }
         vTaskDelay(10 / portTICK_RATE_MS);
